9038aea4c7cf214b077a2334eb8a0c1059d77e34
Sahan Lakmal <sahanloser@gmail.com>
2023-11-12 02:42:52 +0530

    Add UI elements and CSS

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="app-container">
-      
+      <form action="" className="note-form">
+        <input type="text" placeholder='Title'/>
+        <textarea name="" id="" rows={10} placeholder='Content' required></textarea>
+        <button type='submit'>Add Note</button>
+      </form>
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Content</p>
+        </div>
+      </div>
     </div>
   );
 }
