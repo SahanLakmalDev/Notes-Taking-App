d527c62f8fa870b37514e53e3407f38fbe7d711b
Sahan Lakmal <sahanloser@gmail.com>
2023-11-12 03:13:12 +0530

    Add Dummy Data to verify the responsiveness

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,7 +1,40 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+  ])
+
   return (
     <div className="app-container">
       <form action="" className="note-form">
@@ -10,13 +43,15 @@ function App() {
         <button type='submit'>Add Note</button>
       </form>
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
-            <button>x</button>
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>Note Title</h2>
+            <p>Content</p>
           </div>
-          <h2>Note Title</h2>
-          <p>Content</p>
-        </div>
+        ))}  
       </div>
     </div>
   );
