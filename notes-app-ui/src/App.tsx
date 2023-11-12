11386f453e8b09a5eaa801df2b65307865a9a9eb
Sahan Lakmal <sahanloser@gmail.com>
2023-11-12 13:25:58 +0530

    Finalized the project

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -9,36 +9,6 @@ type Note = {
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
-    // {
-    //   id: 1,
-    //   title: "test note 1",
-    //   content: "bla bla note1",
-    // },
-    // {
-    //   id: 2,
-    //   title: "test note 2 ",
-    //   content: "bla bla note2",
-    // },
-    // {
-    //   id: 3,
-    //   title: "test note 3",
-    //   content: "bla bla note3",
-    // },
-    // {
-    //   id: 4,
-    //   title: "test note 4 ",
-    //   content: "bla bla note4",
-    // },
-    // {
-    //   id: 5,
-    //   title: "test note 5",
-    //   content: "bla bla note5",
-    // },
-    // {
-    //   id: 6,
-    //   title: "test note 6",
-    //   content: "bla bla note6",
-    // },
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -82,7 +52,6 @@ function App() {
       
         }
       );
-
       const newNote = await response.json();
       setNotes([newNote,...notes,]);
       setTitle('');
